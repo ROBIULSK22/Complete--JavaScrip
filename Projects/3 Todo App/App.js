1ab73bd132b76dbafd6e534f47003cd162d3facf
ROBIULSK22 <robiulskin25@gmail.com>
2025-11-03 17:35:19 +0530

    complete todo App

--- Projects/3 Todo App/App.js
+++ Projects/3 Todo App/App.js
@@ -1,4 +1,5 @@
 let todoItems = [];
+
 renderTodos();
 function addNewTodo() {
   let todoInput = document.querySelector("#todo-input");
@@ -7,19 +8,21 @@ function addNewTodo() {
   let todoText = todoInput.value.trim();
   todoInput.value = "";
   todoDate.value = "";
-
   if (todoText === "" || dateValue === "") {
     alert("Please fill out both fields!");
     return;
   } else {
     todoItems.push({ item: todoText, Date: dateValue });
   }
+  let saveInputDate = localStorage.setItem("todo", JSON.stringify(todoItems));
+  console.log(saveInputDate);
   renderTodos();
 }
 function renderTodos() {
   let todoContainer = document.querySelector(".todo-container");
   todoContainer.innerHTML = "";
   let newHtml = "";
+  let todoItems = JSON.parse(localStorage.getItem("todo")) || [];
   for (let i = 0; i < todoItems.length; i++) {
     let todoItem = todoItems[i].item;
     let todoDate = todoItems[i].Date;
@@ -27,7 +30,7 @@ function renderTodos() {
         
              <span>${todoItem}</span>
              <span>${todoDate}</span>
-            <button class="delete-todo-btn", onclick="todoItems.splice(${i},1);renderTodos();">Delete</button>
+            <button class="delete-todo-btn", onclick="todoItems.splice(${i},1);renderTodos();localStorage.setItem("todo", JSON.stringify(todoItems));;">Delete</button>
         `;
   }
   todoContainer.innerHTML = newHtml;
